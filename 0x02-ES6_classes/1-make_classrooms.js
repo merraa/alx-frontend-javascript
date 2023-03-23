01ba3d2b76ddba58f4c46000811b016deec73831
meron <104758118+merraa@users.noreply.github.com>
2023-03-23 23:39:47 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
-import classRoom from "./0-classroom"
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const objOne = new classRoom(19);
-  const objTwo = new classRoom(20);
-  const objThree = new classRoom(34);
-  return [objOne, objTwo, objThree]
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
