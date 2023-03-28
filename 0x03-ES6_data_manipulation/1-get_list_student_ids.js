060c0271894c472b1378f5b2c57e894de1735bf6
meron <104758118+merraa@users.noreply.github.com>
2023-03-28 10:45:12 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(arrObj){
-  if(typeof(arrObj) !== 'Array')
-    return [];
-  else 
+  if(typeof(arrObj) === 'Array')
     return arrObj.map(elem => elem.id);
+  else 
+    return [];
 }
