dd1eb004ebdc452b2a30b5e5d55020022a466596
meron <104758118+merraa@users.noreply.github.com>
2023-03-28 11:42:48 +0300

    Create 4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(students, city, newGrade) {
+  const arr = students.filter((obj) => obj.location === city);
+  const newArr = arr.map((obj) => {
+    obj.grade = "N/A";
+    return obj;
+  })
+  for(const i of newGrade){
+    for(const j of newArr){
+      if(j.id === i.studentId){
+        j.grade = i.grade;
+      }
+    }
+  }
+  return ...newArr;
+}
