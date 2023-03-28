7051da1af95362aa9d94efc44e8cc02417d1f7ea
meron <104758118+merraa@users.noreply.github.com>
2023-03-28 10:53:35 +0300

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,3 @@
-import getListStudents from "./0-get_list_students.js";
-
-export default function getStudentsByLocation(getListStudents, city){
-  if(typeof(city) !== "string"){
-    throw new TypeError("city must be a string);
-  }
-  return getListStudents.filter(elem => elem.location === city);
+export default function getStudentsByLocation(Students, city){
+  return Students.filter((elem) => elem.location === city);
 }
