27bcb2295620dc1440e1341d266524c468a32f4a
meron <104758118+merraa@users.noreply.github.com>
2023-03-28 10:54:35 +0300

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(Students, city){
-  return Students.filter((elem) => elem.location === city);
+export default function getStudentsByLocation(students, city) {
+  return students.filter((obj) => obj.location === city);
 }
